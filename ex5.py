a9661f4b89eb18df0825ad9209fb50dade0a5fd1
chloesimhamarratche <chlomarratche@gmail.com>
2026-02-10 11:28:53 +0200

    Python exercices
    Chloe Simha Marratche

---
+++ ex5.py
@@ -0,0 +1,5 @@
+name= "Chloe Simha"
+age= 22
+shoe_size= 39
+info= f"My name is {name}, i am {age} and my shoe size is {shoe_size}."
+print(info)
---
+++ ex9.py
@@ -0,0 +1,5 @@
+userheight= int(input("What's your height in centimeters ?"))
+if userheight >=145:
+    print("You are tall enough to ride !")
+else: 
+    print("You need to grow some more to ride...")
