dbc2db8dc81a1b4d98fe1ebf1622400175e466e9
chloesimhamarratche <chlomarratche@gmail.com>
2026-02-10 11:47:42 +0200

    Delete ex8.py

--- ex8.py
+++
@@ -1,6 +0,0 @@
-myname= "Chloe Simha"
-username= input("What's your name?")
-if myname == username:
-    print("No way, we have the same name !")
-else: 
-    print(f"Nice to meet you {username} !")
